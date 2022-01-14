f37ffbe11017275fc45631373bb4161773f15374
sturmanj <sturmanj@catlin.edu>
2022-01-13 22:09:56 -0800

    :sparkles: made form first draft

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	//http://localhost:8080?userId=1a2b3c&event=BB2022
+
+	import { data } from "./stores.js";
+
+	var autoVal = location.search
+		.slice(1)
+		.split('&')
+		.map(p => p.split('='))
+		.reduce((obj, [key, value]) => ({ ...obj, [key]: value }), {});
+	$data.userId = autoVal.userId
+	$data.event = autoVal.event 
+
+	function submit() {
+		console.log($data)
+		//make the page unusable from here, probably put a close message and chicken animation.
+	}
+</script>
+
+<div>
+	<h1>notes</h1>
+	<textarea placeholder = "Write your notes here..." bind:value={$data.notes} ></textarea>
+	<button on:click={submit}>submit</button>
+</div>
+
+<style>
+	textarea {
+		width: 100%; height: 300px;
+	}
+
+	div {
+  		text-align: center;
+	}
+</style>
+
